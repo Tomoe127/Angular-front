e980188ca3d89fe709056f7a6b066c5517a3d320
Anthony Christian Ramos Rojas <eltanin.pe@gmail.com>
2024-12-10 15:39:28 -0500

    subiendo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'productos',
+        loadComponent: () => import('./product-list/product-list.component')
+    },
+    {
+        path: 'usuarios',
+        loadComponent: () => import('./user-list/user-list.component')
+    },
+    {
+        path: 'new',
+        loadComponent: () => import('./user-form/user-form.component')
+    },
+];
---
+++ src/app/user-list/user-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-user-list',
+  imports: [RouterModule],
+  templateUrl: './user-list.component.html',
+  styleUrl: './user-list.component.css'
+})
+export default class UserListComponent implements OnInit{
+
+  private userService = inject(UserService);
+
+  users: any[] = [];
+
+  ngOnInit(): void {
+      this.userService.listUsers()
+      .subscribe((users: any) => {
+        this.users = users;
+      });
+  }
+} {
+
+}
