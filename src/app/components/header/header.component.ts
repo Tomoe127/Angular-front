62a737cbb1be8c39d6f7c390680fdb02fab885eb
Fufu <fabriziofournier2003@hotmail.com>
2024-12-21 11:10:37 -0500

    Subiendo cambios de login

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core'
+import { Component, inject, type OnInit } from '@angular/core'
 import { RouterLink } from '@angular/router'
 import { UserService } from '../../services/user.service'
 
@@ -8,17 +8,17 @@ import { UserService } from '../../services/user.service'
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
-  private userService = inject(UserService); // Inyecta el servicio
-  userName: string
+export class HeaderComponent implements OnInit {
+  private userService = inject(UserService);
+  userName: string = '';
 
-  constructor() {
-    const currentUser = JSON.parse(localStorage.getItem('currentUser') || '{}')
-    this.userName = currentUser.nombre || 'Invitado' // Asignar el nombre del usuario o 'Invitado'
+  ngOnInit() {
+    this.userService.currentUser$.subscribe(user => {
+      this.userName = user?.nombre || 'Invitado';
+    });
   }
 
-  logout () {
-    this.userService.logout()
+  logout() {
+    this.userService.logout();
   }
-
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,14 +2,22 @@ import { HttpClient } from '@angular/common/http'
 import { inject, Injectable } from '@angular/core'
 import { Router } from '@angular/router'
 import { CarritoService } from './carrito.service'
-import { Observable } from 'rxjs'
+import { BehaviorSubject, Observable } from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private http: HttpClient, private router: Router, private carritoService: CarritoService) { }
+  private currentUserSubject = new BehaviorSubject<any>(null);
+  currentUser$ = this.currentUserSubject.asObservable();
+
+  constructor(private http: HttpClient, private router: Router, private carritoService: CarritoService) { 
+    const storedUser = JSON.parse(localStorage.getItem('currentUser') || 'null');
+    if (storedUser) {
+      this.currentUserSubject.next(storedUser);
+  }
+}
 
   createUser (user: any) {
     return this.http.post('http://localhost:8080/api/registerUsuario', user, { responseType: 'text' })
@@ -21,15 +29,16 @@ export class UserService {
   }
 
   logout () {
-    this.carritoService.limpiarCarrito()
-    localStorage.removeItem('authToken')
-    localStorage.removeItem('currentUser')
-    this.router.navigate(['/login'])
+    this.carritoService.limpiarCarrito();
+    localStorage.removeItem('authToken');
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+    this.router.navigate(['/login']);
   }
-  getUserBySession (email: string) {
-    return this.http.get(`http://localhost:8080/api/getUserBySession?email=${email}`)
-      .subscribe(user => {
-        localStorage.setItem('currentUser', JSON.stringify(user))
-      })
+  getUserBySession(email: string) {
+    return this.http.get(`http://localhost:8080/api/getUserBySession?email=${email}`).subscribe((user: any) => {
+      localStorage.setItem('currentUser', JSON.stringify(user));
+      this.currentUserSubject.next(user);
+    });
   }
 }
